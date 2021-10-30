1b9b09d5f0a869fd64006c4a7265a7733e175fc7
shahabuddin <shahabuddin.idrisi1@srijan.net>
2021-10-30 13:33:00 +0530

    mncscsd

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -8,24 +8,44 @@ module.exports = {
     //   port: 4444,
     //   host: "localhost"
     // },
-    webdriver: {
-      start_process: true,
-      server_path: require("chromedriver").path,
-      port: 9512,
-    },
-    // firefox: {
-    //     // extends: "selenium",
-    //     desiredCapabilities: {
-    //       browserName: "firefox",
-    //       "moz:firefoxOptions": {
-    //         args: ["--headless"],
-    //       },
-   
     test_settings: {
-      default: {    
-        desiredCapabilities: {
-          browserName: 'chrome'
-        }
-      }
-    }
-  };
+        // default: {
+        //   launch_url: process.env.BASE_URL,
+        // },
+        selenium: {
+          selenium: {
+            start_process: true,
+            server_path: require("selenium-server").path,
+            port: 4444,
+            cli_args: {
+              "webdriver.gecko.driver": require("geckodriver").path,
+              "webdriver.chrome.driver": require("chromedriver").path,
+            },
+          },
+          webdriver: {
+            start_process: false,
+          },
+        },
+    
+        chrome: {
+          extends: "selenium",
+          desiredCapabilities: {
+            browserName: "chrome",
+            chromeOptions: {
+              args: ["--headless", "--no-sandbox", "--disable-gpu"],
+              w3c: false,
+            },
+          },
+        },
+    
+        firefox: {
+          extends: "selenium",
+          desiredCapabilities: {
+            browserName: "firefox",
+            "moz:firefoxOptions": {
+              args: ["--headless"],
+            },
+          },
+        },
+      },
+    };
