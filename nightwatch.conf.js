60575c90022d116d5787f41e312a896081782d13
shahabuddin <shahabuddin.idrisi1@srijan.net>
2021-10-30 15:49:23 +0530

    dssc

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -7,29 +7,20 @@ module.exports = {
     //   server_path: "node_modules/chromedriver/lib/chromedriver/chromedriver.exe",
     //   port: 4444,
     //   host: "localhost"
-    // },
+    // },  
     webdriver: {
       start_process: true,
       server_path: require("chromedriver").path,
       port: 9512,
     },
-    // webdriver: {
-    //     start_process : true,
-    //     server_path: "node_modules/.bin/geckodriver",
-    //     cli_args: [
-    //       "--log", "debug"
-    //     ],
-    //     "port": 0
-    //   },
-      "test_settings" : {
-        "default" : {
-          "desiredCapabilities" : {
-            "browserName" : "chrome",
-            "chromeOptions": {
-              "args" : ["--headless","--no-sandbox"]
-            },
-            "loggingPrefs": {"driver": "INFO", "server": "OFF", "browser": "INFO"}
-          }
-        }
-      }
-};
+   
+    test_settings: {
+      default: {    
+        desiredCapabilities: {
+            browserName: 'chrome',
+            chromeOptions: {
+              args: ['--headless', '--no-sandbox']
+          }  }
+       } 
+    }
+  };
