6656ba41c620012bc8494e270405b6e76774f37c
shahabuddin <shahabuddin.idrisi1@srijan.net>
2021-10-30 13:12:33 +0530

    mncscsd

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -10,14 +10,21 @@ module.exports = {
     // },
     webdriver: {
       start_process: true,
-      server_path: require("chromedriver").path,
+      server_path: require("firefoxdriver").path,
       port: 9512,
     },
+    // firefox: {
+    //     // extends: "selenium",
+    //     desiredCapabilities: {
+    //       browserName: "firefox",
+    //       "moz:firefoxOptions": {
+    //         args: ["--headless"],
+    //       },
    
     test_settings: {
       default: {    
         desiredCapabilities: {
-          browserName: 'chrome'
+          browserName: 'firefox'
         }
       }
     }
