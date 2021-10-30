6a415887e64f487e59afb3cff4bd883f21e72f62
shahabuddin <shahabuddin.idrisi1@srijan.net>
2021-10-30 18:14:36 +0530

    chcd

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -9,18 +9,20 @@ module.exports = {
     //   host: "localhost"
     // },  
     webdriver: {
-      start_process: true,
-      server_path: require("chromedriver").path,
-      port: 9512,
-    },
-   
-    test_settings: {
-      default: {    
-        desiredCapabilities: {
-            browserName: 'chrome',
-            chromeOptions: {
-              args: ['--headless', '--no-sandbox']
-          }  }
-       } 
-    }
+        start_process: true,
+        server_path: require("chromedriver").path,
+        port: 9512,
+      },
+     
+      test_settings: {
+        default: {    
+            desiredCapabilities: {
+                browserName: 'chrome',
+                chromeOptions: {
+                  args: ['--headless', '--no-sandbox']
+                }
+            },
+            
+        }
+      }
   };
