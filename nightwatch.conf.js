77b3289cfc6000fbd1ce1c30b077e8809788a1ba
shahabuddin <shahabuddin.idrisi1@srijan.net>
2021-10-30 18:48:01 +0530

    cascasc

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -8,30 +8,44 @@ module.exports = {
     //   port: 4444,
     //   host: "localhost"
     // },  
-    webdriver: {
-        start_process: true,
-        server_path: require("chromedriver").path,
-        port: 9512,
-      },
-      disable_error_log: false,
-      launch_url: 'https://nightwatchjs.org',
-    
-      screenshots: {
-        enabled: false,
-        path: 'screens',
-        on_failure: true
-      },
-    
-     
-      test_settings: {
-        default: {    
-            desiredCapabilities: {
-                browserName: 'chrome',
-                chromeOptions: {
-                  args: ['--headless', '--no-sandbox']
-                }
+    test_settings: {
+        default: {
+          launch_url: process.env.BASE_URL,
+        },
+        selenium: {
+          selenium: {
+            start_process: true,
+            server_path: require("selenium-server").path,
+            port: 4444,
+            cli_args: {
+              "webdriver.gecko.driver": require("geckodriver").path,
+              "webdriver.chrome.driver": require("chromedriver").path,
             },
-            
-        }
-      }
+          },
+          webdriver: {
+            start_process: false,
+          },
+        },
+    
+        chrome: {
+          extends: "selenium",
+          desiredCapabilities: {
+            browserName: "chrome",
+            chromeOptions: {
+              args: ["--headless", "--no-sandbox", "--disable-gpu"],
+              w3c: false,
+            },
+          },
+        },
+    
+        firefox: {
+          extends: "selenium",
+          desiredCapabilities: {
+            browserName: "firefox",
+            "moz:firefoxOptions": {
+              args: ["--headless"],
+            },
+          },
+        },
+      },
   };
