2766825867541a883a31c54b281625b737e7a564
shahabuddin <shahabuddin.idrisi1@srijan.net>
2021-10-30 13:43:01 +0530

    update

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -8,44 +8,23 @@ module.exports = {
     //   port: 4444,
     //   host: "localhost"
     // },
-    test_settings: {
-        // default: {
-        //   launch_url: process.env.BASE_URL,
-        // },
-        selenium: {
-          selenium: {
-            start_process: true,
-            server_path: require("selenium-server").path,
-            port: 4444,
-            cli_args: {
-              "webdriver.gecko.driver": require("geckodriver").path,
-              "webdriver.chrome.driver": require("chromedriver").path,
-            },
-          },
-          webdriver: {
-            start_process: false,
-          },
-        },
-    
-        chrome: {
-          extends: "selenium",
-          desiredCapabilities: {
-            browserName: "chrome",
-            chromeOptions: {
-              args: ["--headless", "--no-sandbox", "--disable-gpu"],
-              w3c: false,
-            },
-          },
-        },
-    
-        firefox: {
-          extends: "selenium",
-          desiredCapabilities: {
-            browserName: "firefox",
-            "moz:firefoxOptions": {
-              args: ["--headless"],
-            },
-          },
-        },
+    desiredCapabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+          args: ['--headless', '--no-sandbox']
+        }
       },
-    };
+    webdriver: {
+      start_process: true,
+      server_path: require("chromedriver").path,
+      port: 9512,
+    },
+   
+    test_settings: {
+      default: {    
+        desiredCapabilities: {
+          browserName: 'chrome'
+        }
+      }
+    }
+  };
